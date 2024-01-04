a643c3008bfce42a01d12f5dcde70d2726d14d6e
Hadjaj <ratiba14@yahoo.fr>
2024-01-04 10:32:53 +0100

    add

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var app = express();
 //Middleware pour recuperer la donnée
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //fichier de configarition
 require('dotenv').config();
 
 // var path = require('path');
 //Connexion mongodb :
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 const url = process.env.DATABASE_URL;
 
 mongoose.connect(url)
-.then(console.log("Mongodb connected"))
-.catch(err => console.log(err));
+    .then(console.log("Mongodb connected"))
+    .catch(err => console.log(err));
 
 //Systeme de vue EJS
 app.set('view engine', 'ejs');
@@ -25,7 +25,7 @@ const cors = require('cors');
 //De base
 // app.use(cors());
 //Transmettre TOUT type de données meme sensible (JWT)
-app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 
 //Method put et delete pour express
@@ -41,41 +41,41 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
 //Import JWT
-const {createTokens, validateToken} = require('./JWT');
+const { createTokens, validateToken } = require('./JWT');
 
 //Import jwt decode
-const {jwtDecode} = require('jwt-decode');
+const { jwtDecode } = require('jwt-decode');
 
 //Multer 
 const multer = require('multer')
 app.use(express.static('uploads'));
 
 const storage = multer.diskStorage({
-    destination : (req, file, cb) =>{
+    destination: (req, file, cb) => {
         cb(null, 'uploads/')
     },
-    filename : (req, file, cb) =>{
+    filename: (req, file, cb) => {
         cb(null, file.originalname);
     }
 })
-const upload = multer({storage})
+const upload = multer({ storage })
 
 
 //Models :
 
 var Contact = require('./models/Contact');
 
-app.post('/uploadimage', upload.single('image'), function(req, res){
-    if(!req.file){
+app.post('/uploadimage', upload.single('image'), function (req, res) {
+    if (!req.file) {
         res.status(400).json("No file uploaded!");
     }
-    else{
+    else {
         res.json("File uploaded!");
     }
 })
 
-app.post('/uploadmultiple', upload.array('images', 5), function(req, res) {
-    if(!req.files || req.files.length === 0) {
+app.post('/uploadmultiple', upload.array('images', 5), function (req, res) {
+    if (!req.files || req.files.length === 0) {
         res.status(400).json("No files uploaded!");
     }
     else {
@@ -84,228 +84,227 @@ app.post('/uploadmultiple', upload.array('images', 5), function(req, res) {
 });
 
 //Contact
-app.get('/', validateToken, function(req, res){
+app.get('/', validateToken, function (req, res) {
     // res.sendFile(path.resolve('index.html'));
     Contact.find()
-    .then((data)=>{
-        console.log(data);
-        // res.render('Home', {data: data});
-        res.json(data);
-    })
-    .catch(err => console.log(err));
+        .then((data) => {
+            console.log(data);
+            // res.render('Home', {data: data});
+            res.json(data);
+        })
+        .catch(err => console.log(err));
 });
 
-app.get('/newContact', function(req, res){
+app.get('/newContact', function (req, res) {
     res.render('NewContact');
 });
 
-app.get('/contact/:id', function(req, res){
+app.get('/contact/:id', function (req, res) {
     Contact.findOne({
-        _id : req.params.id
+        _id: req.params.id
     })
-    .then((data)=>{
-        // res.render('Edit', {data: data});
-        res.json(data);
-    })
-    .catch(err => console.log(err));
+        .then((data) => {
+            // res.render('Edit', {data: data});
+            res.json(data);
+        })
+        .catch(err => console.log(err));
 });
 
-app.post('/submit-data', function(req, res){
+app.post('/submit-data', function (req, res) {
     var name = req.body.firstname + ' ' + req.body.lastname;
     res.send(name + ' Submitted successfully');
 })
 
-app.post('/submit-contact', function(req, res){
+app.post('/submit-contact', function (req, res) {
     const Data = new Contact({
-        nom : req.body.nom,
-        prenom : req.body.prenom,
-        email : req.body.email,
-        message : req.body.message
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        email: req.body.email,
+        message: req.body.message
     })
 
     Data.save()
-    .then(()=>{
-        console.log("Data saved successfully");
-        res.redirect('/');
-    })
-    .catch(err=>{
-        console.log(err);
-    });
+        .then(() => {
+            console.log("Data saved successfully");
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-app.put('/edit/:id', function(req, res){
+app.put('/edit/:id', function (req, res) {
     const Data = {
-        nom : req.body.nom,
-        prenom : req.body.prenom,
-        email : req.body.email,
-        message : req.body.message
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        email: req.body.email,
+        message: req.body.message
     }
-    Contact.updateOne({_id : req.params.id}, {$set: Data})
-    .then(()=>{
-        console.log("Data updated successfully");
-        res.redirect('/'); 
-    })
-    .catch(err=>{console.log(err);});
+    Contact.updateOne({ _id: req.params.id }, { $set: Data })
+        .then(() => {
+            console.log("Data updated successfully");
+            res.redirect('/');
+        })
+        .catch(err => { console.log(err); });
 })
 
-app.delete('/delete/:id', function(req, res){
+app.delete('/delete/:id', function (req, res) {
     Contact.findOneAndDelete({
-        _id : req.params.id
+        _id: req.params.id
     })
-    .then(()=>{
-        console.log("Data deleted successfully");
-        res.redirect('/'); 
-    })
-    .catch(err=>{console.log(err);});
+        .then(() => {
+            console.log("Data deleted successfully");
+            res.redirect('/');
+        })
+        .catch(err => { console.log(err); });
 })
 
 //Blog
 var Blog = require('./models/Blog');
 //affichage formulaire nouveau blog
-app.get('/newblog', function(req, res){
+app.get('/newblog', function (req, res) {
     res.render('NewBlog')
 });
 
 //Ajout d'un blog
-app.post('/addblog', upload.single('image') ,function(req, res){
+app.post('/addblog', upload.single('image'), function (req, res) {
     const Data = new Blog({
-        titre : req.body.titre,
-        sousTitre : req.body.sousTitre,
-        auteur : req.body.auteur,
-        description : req.body.description,
-        imageName : req.file.filename,
-        datePublication : req.body.datePublication
+        titre: req.body.titre,
+        sousTitre: req.body.sousTitre,
+        auteur: req.body.auteur,
+        description: req.body.description,
+        imageName: req.file.filename,
+        datePublication: req.body.datePublication
     })
     // console.log(req.file);
 
     //Image obligatoire pour l'enregistrement d'un blog
-    if(!req.file){
+    if (!req.file) {
         res.status(400).json("No File Uploaded")
     }
-    else{
+    else {
 
         Data.save()
-        .then(() =>{
-            console.log("Blog saved");
-            res.json("Blog saved")
-            // res.redirect('http://localhost:3000/allblogs/')
-        })
-        .catch(err =>console.error(err));
+            .then(() => {
+                console.log("Blog saved");
+                res.json("Blog saved")
+                // res.redirect('http://localhost:3000/allblogs/')
+            })
+            .catch(err => console.error(err));
 
     }
 });
 
 //recuperation de les blogs
-app.get('/allblogs', function(req, res){
+app.get('/allblogs', function (req, res) {
     Blog.find()
-    .then((data)=>{
-        // res.render('AllBlogs',{data:data});
-        res.json(data);
-    })
+        .then((data) => {
+            // res.render('AllBlogs',{data:data});
+            res.json(data);
+        })
 });
 //page qui affiche le formulaire d'edition
-app.get('/blog/:id', function(req, res){
+app.get('/blog/:id', function (req, res) {
     Blog.findOne({
-        _id : req.params.id
+        _id: req.params.id
     })
-    .then((data)=>{
-        // res.render('EditBlog',{data:data});
-        res.json(data);
-    })
+        .then((data) => {
+            // res.render('EditBlog',{data:data});
+            res.json(data);
+        })
 });
 
 //Update
-app.put('/editblog/:id', function(req, res){
+app.put('/editblog/:id', function (req, res) {
     const Data = {
-        titre : req.body.titre,
-        sousTitre : req.body.sousTitre,
-        auteur : req.body.auteur,
-        description : req.body.description
+        titre: req.body.titre,
+        sousTitre: req.body.sousTitre,
+        auteur: req.body.auteur,
+        description: req.body.description
     }
 
     Blog.updateOne({
-        _id : req.params.id
-    }, {$set:Data})
-    .then(()=>{
-        res.redirect(process.env.FRONTEND_URL + '/allblogs')
-    })
-    .catch((err)=>{
-        console.log(err);
-    }); 
+        _id: req.params.id
+    }, { $set: Data })
+        .then(() => {
+            res.redirect(process.env.FRONTEND_URL + '/allblogs')
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
-app.delete('/deleteblog/:id', function(req, res) {
-    Blog.findOneAndDelete({_id:req.params.id})
-    .then(()=>{
-        console.log("Blog deleted");
-        res.redirect(process.env.FRONTEND_URL + '/allblogs');
-    })
-    .catch((err)=>{console.log(err);})
+app.delete('/deleteblog/:id', function (req, res) {
+    Blog.findOneAndDelete({ _id: req.params.id })
+        .then(() => {
+            console.log("Blog deleted");
+            res.redirect(process.env.FRONTEND_URL + '/allblogs');
+        })
+        .catch((err) => { console.log(err); })
 });
 
 //User
 const User = require('./models/User');
 
 //Inscription 
-app.post('/api/inscription', function(req, res){
+app.post('/api/inscription', function (req, res) {
     const Data = new User({
-        username : req.body.username,
-        email : req.body.email,
-        password : bcrypt.hashSync(req.body.password, 10),
-        admin : req.body.admin
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        admin: req.body.admin
     })
 
     Data.save()
-    .then(()=>{
-        console.log("User saved !");
-        res.redirect('/signin');
-    })
-    .catch(err=>{console.log(err);});
+        .then(() => {
+            console.log("User saved !");
+            res.redirect('/signin');
+        })
+        .catch(err => { console.log(err); });
 })
 
-app.get('/signup', function(req, res){
+app.get('/signup', function (req, res) {
     res.render('Inscription');
 })
-app.get('/signin', function(req, res){
+app.get('/signin', function (req, res) {
     res.render('Connexion');
 })
 
-app.post('/api/connexion', function(req, res){
+app.post('/api/connexion', function (req, res) {
     User.findOne({
-        username : req.body.username
-    }).then(user =>{
-        if(!user)
-        {
+        username: req.body.username
+    }).then(user => {
+        if (!user) {
             return res.status(404).send("No user found");
         }
-        if(!bcrypt.compareSync(req.body.password, user.password)){
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
             return res.status(404).send("Invalid password");
         }
-        
+
         const accessToken = createTokens(user)
         res.cookie("access-token", accessToken, {
             maxAge: 1000 * 60 * 60 * 24 * 30, //30 jours en ms
             httpOnly: true
-        } )
+        })
 
-        res.redirect("http://localhost:3000/");
+        res.redirect(process.env.FRONTEND_URL);
         // res.json('LOGGED IN');
 
         // res.render('UserPage', {data : user})
     })
-    .catch(err =>{console.log(err);});
+        .catch(err => { console.log(err); });
 
 });
 
-app.get('/logout', (req, res) =>{
+app.get('/logout', (req, res) => {
     res.clearCookie("access-token");
-    res.redirect('http://localhost:3000/');
+    res.redirect(process.env.FRONTEND_URL);
 })
 
-app.get('/getJwt', validateToken, (req, res) =>{
+app.get('/getJwt', validateToken, (req, res) => {
     res.json(jwtDecode(req.cookies["access-token"]))
 });
 
-var server = app.listen(5000, function() {
+var server = app.listen(5000, function () {
     console.log("Server listening on port 5000");
 });
